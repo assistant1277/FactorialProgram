7a45728a918e0f2b4b0069fc82da8dafbc7f6975
assistant1277 <shivarajghangale2002@gmail.com>
2024-09-25 14:27:17 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using FactorialProgram.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number to get factorial ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Factorial.CheckAndPrintFactorial(inputNumber);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ model/Factorial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProgram.model
+{
+    public class Factorial
+    {
+        public static int CalculateFactorialRecursive(int number)
+        {
+            if(number <= 1)
+                return 1;
+            return number * CalculateFactorialRecursive(number - 1);
+        }
+        public static int CalculateFactorialIterative(int number)
+        {
+            int factorial = 1;
+            for(int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+        public static void CheckAndPrintFactorial(int number)
+        {
+            int factorialRecursive = CalculateFactorialRecursive(number);
+            Console.WriteLine($"factorial of {number} with recursive approach is {factorialRecursive}");
+
+            int factorialIterative = CalculateFactorialIterative(number);
+            Console.WriteLine($"factorial of {number} with iterative approach is {factorialIterative}");
+        }
+    }
+}
